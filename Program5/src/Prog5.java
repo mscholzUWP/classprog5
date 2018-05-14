a503a1c1b4a50960f57c3c46fa203a4a9afc23a6
Unknown <scholzm@uwplatt.edu>
2018-05-13 21:06:26 -0500

    move files to main package
    move main files to main package so i can reference them

--- Program5/src/Prog5.java
+++ Program5/src/Prog5.java
@@ -1,3 +1,6 @@
+
+import main.BankAccountManagement;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
